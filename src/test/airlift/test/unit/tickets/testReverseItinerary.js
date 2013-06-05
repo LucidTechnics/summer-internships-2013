edab6f89319e5ec8f1ea081c7f98cf3400e27f7f
Sina Viseh <sinaviseh@Sinas-MacBook-Pro.local>
2013-06-05 14:13:30 -0400

    read

--- src/test/airlift/test/unit/tickets/testReverseItinerary.js
+++ src/test/airlift/test/unit/tickets/testReverseItinerary.js
@@ -1,8 +1,49 @@
+
+
 exports['test reverse itinerary'] = function(_assert)
 {
 	var tickets = [['ALB','BWI'],['LAX','IAD'],['BWI','LAX'],['IAD','GA']];
-	var reversed = [];
+  	var reversed = [];
+    var start;
+    reversed=reverse(tickets);
+    _assert.eq(JSON.stringify(reversed), JSON.stringify([['GA','IAD'],['IAD','LAX'],['LAX','BWI'],['BWI','ALB']]), 'it worked');
+function reverse(_tickets)
+{
+    var reversedTickets = [];
 
-	_assert.eq(reversed, [['GA','IAD'],['IAD','LAX'],['LAX','BWI'],['BWI','ALB']], 'test reversed itinerary failed');
+      var s=[];
+      var map=[];
+       
+	for(var i=0;i<tickets.length;i++)
+	{
+	   // reversedTickets[i]=[tickets[i][1],tickets[i][0]] ;
+	  //  map[reversedTickets[i][0]]=reversedTickets[i][1];
+	   // s[reversedTickets[i][1]]=1;
+//
+           map[tickets[i][1]]=tickets[i][0];
+	    s[tickets[i][0]]=1;
+	}
 
+	for(i=0;i<tickets.length;i++)
+	{
+	  //if(s[reversedTickets[i][0]]!=1)
+	    if(s[tickets[i][1]]!=1)
+	  {
+           // start=reversedTickets[i][0];
+	      start=tickets[i][1];
+	    break;
+	    }
+	}
+
+	for(i=0;i<tickets.length;i++)
+	{
+	      // reversedTickets[i][0]=start;
+	//	reversedTickets[i][1]=map[start];
+	    reversedTickets[i]=[start,map[start]];
+		start=reversedTickets[i][1];
+	}
+
+
+    return reversedTickets;
+}
 };
