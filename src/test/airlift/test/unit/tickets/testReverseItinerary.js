ec6acff2f17f5f1f2440e64423a8564cf5e3d478
jdwilliams15 <jwsorad@gmail.com>
2013-06-05 15:49:51 -0400

    booga booga

--- src/test/airlift/test/unit/tickets/testReverseItinerary.js
+++ src/test/airlift/test/unit/tickets/testReverseItinerary.js
@@ -1,1 +1,46 @@
+exports['test reverse itinerary'] = function(_assert)
+{
+    var tickets = [['ALB','BWI'],['LAX','IAD'],['BWI','LAX'],['IAD','GA']];
+    
+    //var reversed = [];
+    //  _assert.eq(JSON.stringify([['GA','IAD'],['IAD','LAX'],['LAX','BWI'],['BWI','ALB']]), JSON.stringify([['GA','IAD'],['IAD','LAX'],  ['LAX','BWI'],['BWI','ALB']]), 'test reversed itinerary failed');
 
+    //_assert.eq(true, true, "failed");
+    
+    _assert.eq(JSON.stringify(reversed(tickets)), JSON.stringify([['GA','IAD'],['IAD','LAX'],['LAX','BWI'],['BWI','ALB']]), 'test reversed itinerary failed');
+
+};
+
+
+
+function reversed(t) {
+    for (var i = 0; i < t.length; i++ ) {
+	var tmp = t[i][0];
+	t[i][0] = t[i][1];
+	t[i][1] = tmp;
+    }
+    t = sort(t);
+    return t;
+}
+
+
+function sort(t) {
+    var tt = [];
+    tt.push(t[0][0]);
+    tt.push(t[0][1]);
+
+    for (var i = 0; i < t.length; i++) {
+	for (var j = 0; j < t.length; j++) {
+	    if (t[j][1] === tt[0]) {
+		tt.unshift(t[j][0]);
+	    } else if (t[j][0] === tt[tt.length - 1]) {
+		tt.push(t[j][1]);
+	    }
+	}
+    }
+    for (var i = 0; i < tt.length - 1; i++ ) {
+	t[i][0] = tt[i];
+	t[i][1] = tt[i + 1];
+    }
+    return t;
+}
